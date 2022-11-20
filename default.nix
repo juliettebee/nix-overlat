91aa2c408011806780ee87474687a90bb8f3275a
Juliette Benrheisel <git@juliette.page>
2022-11-20 00:44:13 -0500

    Fix?

--- default.nix
+++ default.nix
@@ -1,11 +1,1 @@
-let
-  battery-buddy = pkgs.callPackage ./battery-buddy.nix {};
-  pkgs = import sources.nixpkgs {
-    overlays = [
-      (self: super: {
-        inherit battery-buddy;
-      })
-    ];
-  };
-
-in pkgs
+{}
